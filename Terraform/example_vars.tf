c772f3ce6efae39c29931b073d99ca0213e2ee13
SmigorX <igorjaniak@gmail.com>
2025-01-09 09:13:35 +0100

    Provisioning and setup base

---
+++ Terraform/example_vars.tf
@@ -0,0 +1,16 @@
+variable "ssh_key" {
+  default = "ssh key"  
+}
+
+variable "url" {
+  default = "endpoint url"
+}
+
+variable "pm_api_key" {
+    default = "proxmox api key"
+}
+
+variable "proxmox_host" {
+    default = "host name"
+}
+
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -14,36 +14,75 @@ provider "proxmox" {
   pm_tls_insecure = true
 }
 
+variable "mem_size" {
+  description = "Amount of RAM (in MiB) for the virtual machine"
+  type        = string
+  default     = "2048"
+}
+
+variable "cpu_cores" {
+  description = "Number of CPU cores for the virtual machine"
+  type        = number
+  default     = 1
+}
+
+variable "vm_count" {
+  description = "Number of virtual machines to create"
+  type        = number
+  default     = 2
+}
+
+variable "template_name" {
+  description = "Name of the template VM to clone"
+  type        = string
+  default     = "alma-cloud-init"
+}
+
+variable "cloud_init_drive" {
+  description = "Name of the cloud-init drive"
+  type        = string
+  default     = "cloudinit"
+}
+
 resource "proxmox_vm_qemu" "test_server" {
   count       = var.vm_count
   name        = "k8s-vm-${count.index + 1}"
   target_node = var.proxmox_host
+  memory      = var.mem_size
+  cores       = var.cpu_cores
   clone       = var.template_name
-  bootdisk = "scsi0"
-
-  scsihw = "virtio-scsi-pci"
 
   disks {
-    scsi {
-      scsi0 {
-        disk {
-          size = "24G"
-          storage = "local-lvm"
+    ide {
+      ide0 {
+          disk {
+        storage = "local-lvm"
+        size    = "12G"
         }
+      }
+      ide1 {
+          cloudinit{
+        storage = "local-lvm"
+          }
       }
     }
   }
 
   network {
     id = 0
-    model = "virtio"
+    model  = "virtio"
     bridge = "vmbr0"
   }
 
-  tags = "k8s"
+  ipconfig0 = "ip=192.168.50.${count.index + 100}/24"
 
-  sshkeys = <<EOF
-  ${var.ssh_key}
-  EOF
+  tags = "k8s,terraform"
 
+  ciuser = "user"
+  sshkeys = var.ssh_key
 }
+
+output "vm_ip" {
+  value = [for vm in proxmox_vm_qemu.test_server : vm.ipconfig0]
+}
+
